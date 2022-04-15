a66c8d8b4bfef3967fc39a95400c8d421e73ebb3
sebZi <sebzi@gmail.com>
2022-04-15 22:10:13 +0200

    Removed comment line from Open-File func

--- functions/00Files.ps1
+++ functions/00Files.ps1
@@ -162,7 +162,6 @@ function Global:Open-File{
     [Alias("of")]
     param([Parameter(Position=0)][string]$f) 
         if(!$f){return}
-        #$f=Parse-Filename $f
         $CheckEditor=Get-Command -CommandType Application | ? { $_.Name -eq 'powershell_ise.exe' -or $_.Source -like '*powershell*ise.exe'}
         if(!$CheckEditor){
             $CheckEditor="notepad.exe"
